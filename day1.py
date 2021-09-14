76e8e804fb4c0436fedb5982ee02b77be806341e
HKRliduan <90590239+HKRliduan@users.noreply.github.com>
2021-09-14 16:08:13 +0800

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,18 +1,4 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print("hello,world")
 print("================李端的商城================")
 print("日期     服装名称   价格/件  库存数量  销售量/每日")
 print("	1号     羽绒服	  253.6	  500	10")
