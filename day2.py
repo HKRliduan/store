1ba885a21c603be0e4620f25f9176ff0f147f6c5
HKRliduan <90590239+HKRliduan@users.noreply.github.com>
2021-09-15 17:22:44 +0800

    Add files via upload

--- day2.py
+++ day2.py
@@ -1,11 +1,10 @@
 import random
-print("==============猜字游戏=============")
+print("================猜字游戏==============")
 print("猜对加150元，猜错减100元，数字在0-50之间哦！")
 print("首充100元，送初始金额:500元")
 ran = random.randint(0, 50)
 A = 500
 i = 1
-print(ran)
 while i <= 3 or A < 00:
     num = int(input("请输入一个数字:"))
     if num == ran:
